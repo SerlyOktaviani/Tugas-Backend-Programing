cde42e300f3b292897e88c036ad089c60c1cd3a8
SerlyOktaviani19126 <72858044+SerlyOktaviani@users.noreply.github.com>
2021-11-19 16:54:10 +0700

    Update api.php

--- api.php
+++ api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimalController;
 use App\Http\Controllers\StudentController;  
+use App\Http\Controllers\PasienController;  
 
 /*
 |--------------------------------------------------------------------------
@@ -24,15 +25,24 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 #method get
 Route::get('animals', [AnimalController::class, 'index']);
 Route::get('student', [StudentController::class, 'index']);
+Route::get('pasien', [PasienController::class, 'index']);
+
+#pasien  berdasarkan status
+Route::get('pasien/dead', [PasienController::class, 'dead']);
+Route::get('pasien/positive', [PasienController::class, 'positive']);
+Route::get('pasien/recovered', [PasienController::class, 'recovered']);
 
 #method post
 Route::post('animals', [AnimalController::class, 'store']);
 Route::post('student', [StudentController::class, 'store']);
+Route::post('pasien', [PasienController::class, 'store']);
 
 #method put
 Route::put('animals/{id}', [AnimalController::class, 'update']);
 Route::put('student/{id}', [StudentController::class, 'update']);
+Route::put('pasien/{id}', [PasienController::class, 'update']);
 
 #method delete
 Route::delete('animals/{id}', [AnimalController::class, 'destroy']);
 Route::delete('student/{id}', [StudentController::class, 'destroy']);
+Route::delete('pasien/{id}', [PasienController::class, 'destroy']);
